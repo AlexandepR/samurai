95611933898b76339061f9c1fc963bfe498ad34c
Alex <alexandepp@gmail.com>
2021-07-22 10:17:03 +0300

    22jul

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return <header className='header'>
+        <img
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/42093/pouting-cat-emoji-clipart-sm.png"/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+  return <div className='content'>
+      <img
+          alt=''
+          src="https://www.digiseller.ru/preview/917746/p1_2822191_baa2d3c2.jpg"/>
+      <div>
+          ava + description
+      </div>
+      <div>
+          My posts
+          <div>
+              New post
+          </div>
+          <div>
+              <div>
+                  post 1
+              </div>
+              <div>
+                  post 2
+              </div>
+          </div>
+      </div>
+  </div>
+}
+
+export default Profile;
